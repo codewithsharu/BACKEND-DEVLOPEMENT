5b3896429e370fe7b7e2d108e14ce710edb8e09d
Shareen <codewithsharu@gmail.com>
2024-01-20 06:18:34 +0530

    basic rest api for displaying post and content in it

--- REST-ful-API/index.js
+++ REST-ful-API/index.js
@@ -2,22 +2,29 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-//this line is to help express to understand the data from request 
-app.use(express.urlencoded({extended:true}));
+//express ki direct data ardam kadhu so midleware vadi encode chestam recieve ina data ni 
+app.use(express.urlencoded({ extended: true }));
 
 let port = 8989;
 
-app.listen(port,()=>{
-    console.log("app listining at "+port);
+let posts = [
+    { username: 'boolsui', content: 'burrapadu ra babu burra poyindii' },
+    { username: 'tirnimla', content: 'kg nimm kayala 100 rupees konte konu ledante mingey' },
+    { username: 'krinji', content: 'chootu bolthe call me chotuu' }
+];
+
+app.listen(port, () => {
+    console.log("app listening at " + port);
 });
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("yeahh !! working great ");
 });
 
-
-
+app.get("/posts", (req, res) => {
+    res.render('posts.ejs', { posts });
+});
