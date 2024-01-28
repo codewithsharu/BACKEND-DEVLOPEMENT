43699541cdb8b306b11c285ae061dbff4f16c96b
Shareen <codewithsharu@gmail.com>
2024-01-28 15:46:23 +0530

    generating

--- MY-SQL/ShareenDATABASE/index.js
+++ MY-SQL/ShareenDATABASE/index.js
@@ -1,15 +1,12 @@
 const { faker } = require('@faker-js/faker');
 
-let createRandomUser = ()=>{
+let getRandomUser = ()=>{
   return {
     userId: faker.string.uuid(),
     username: faker.internet.userName(),
     email: faker.internet.email(),
-    avatar: faker.image.avatar(),
     password: faker.internet.password(),
-    birthdate: faker.date.birthdate(),
-    registeredAt: faker.date.past(),
   };
 }
 
-console.log(createRandomUser());
+console.log(getRandomUser());
