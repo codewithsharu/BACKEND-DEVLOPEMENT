3261682c6588af01d2028b235d966cb5c51cc097
Shareen <codewithsharu@gmail.com>
2024-02-04 02:01:49 +0530

    passing

--- REACT-js/shareen-app/src/main.jsx
+++ REACT-js/shareen-app/src/main.jsx
@@ -4,12 +4,22 @@ import App from './App.jsx';
 import './index.css';
 
 let o = ['x', 'y', 'z'];
-
+let dic = {
+  x: 'value_x',
+  y: 'value_y',
+  z: 'value_z'
+};
 ReactDOM.render(
   <React.StrictMode>
     <App title="shareen ra babu" sec="A" option={o} />
-    <App title="shareen" sec="B" />
-    <App title="shareen 1234" sec="C" />
+    <App title="shareen" sec="B"option={dic.x}  />
+    <App title="shareen 1234" sec="C" option={dic.y} />
+    <App title="shareen ra babu" sec="A" option={o} />
+    <App title="shareen" sec="B"option={dic.x}  />
+    <App title="shareen 1234" sec="C" option={dic.y} />
+    <App title="shareen ra babu" sec="A" option={o} />
+    <App title="shareen" sec="B"option={dic.x}  />
+    <App title="shareen 1234" sec="C" option={dic.y} />
   </React.StrictMode>,
   document.getElementById('root')
 );
