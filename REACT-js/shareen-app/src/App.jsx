4012db38bae6418f1f4c7cb211c89c5194d7abe5
Shareen <codewithsharu@gmail.com>
2024-02-04 11:46:03 +0530

    giving

--- REACT-js/shareen-app/src/App.jsx
+++ REACT-js/shareen-app/src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App(props) {
+  let s = { backgroundColor: "blue" }; 
   console.log(props);
   return (
-    <div className="card">
+    <div className="card" style={s}>
       <h1>{props.title}</h1>
       <h1>{props.sec}</h1>
       <h1>{props.option}</h1>
